5827b2746214dd50347ebee6565c3d147fc62ffb
vinitgarv <gargvini3456@gmail.com>
2025-04-10 17:44:22 +0530

    Updated Something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { Navigation } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],[LoginComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tut';
-  name='vinit';
-  x=10;
-  y=20;
+
+  otherFunction() {
+    console.log("other");
+  }
+
+  handleClickEvent() {
+    console.log("Button Clicked");
+
+    this.otherFunction();
+  }
   
 }
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-navigation',
+    //template: '<h1>Navigation</h1>',
+    //styles: 'h1{color:orange}'
+    templateUrl:'./navigation.component.html',
+    styleUrl:'./navigation.component.css'
+})
+
+
+export class navigation {
+
+}
